53bd3cd6c1fdd5f21892661a587960a9727b8f10
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-11-28 15:08:06 +0100

    setup project with templates

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use clap::Command;
+use chrono::Datelike;
+use clap::{builder::OsStr, Arg, Command};
 mod setup;
 
-fn main()
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>
 {
     let mut cmd = Command::new("cargo-aoc")
         .author("Sebastian, seblyng98@gmail.com")
         .author("Sivert, sivert-joh@hotmail.com")
-        .subcommand(clap::command!("setup"));
+        .subcommand(
+            clap::command!("setup").arg(
+                Arg::new("year")
+                    .short('y')
+                    .default_value(OsStr::from(chrono::Utc::now().year().to_string())),
+            ),
+        );
 
     let help = cmd.render_help();
     let matches = cmd.get_matches();
     match matches.subcommand()
     {
-        Some(("setup", matches)) => setup::setup(),
+        Some(("setup", matches)) =>
+        {
+            setup::setup(matches).await.expect("Couldn't setup project properly")
+        },
         _ =>
         {
             println!("{}", help);
         },
     }
+    Ok(())
 }
---
+++ template/template.rs
@@ -0,0 +1,59 @@
+fn read_input<T, P>(path: P) -> Vec<T>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+    P: AsRef<std::path::Path>,
+{
+    use std::io::BufRead;
+    let file = std::fs::File::open(path).unwrap();
+    std::io::BufReader::new(file)
+        .lines()
+        .flatten()
+        .flat_map(|s| T::from_str(&s))
+        .collect()
+}
+
+enum Task
+{
+    One,
+    Two,
+}
+
+fn time<F, T, U>(task: Task, f: F, arg: T)
+where
+    F: Fn(T) -> U,
+    U: std::fmt::Display,
+{
+    let t = std::time::Instant::now();
+    let res = f(arg);
+    let elapsed = t.elapsed().as_millis();
+
+    match task
+    {
+        Task::One =>
+        {
+            println!("({}ms)\tTask one: \x1b[0;34;34m{}\x1b[0m", elapsed, res);
+        },
+        Task::Two =>
+        {
+            println!("({}ms)\tTask two: \x1b[0;33;10m{}\x1b[0m", elapsed, res);
+        },
+    };
+}
+
+fn main()
+{
+    let input = read_input("input");
+    time(Task::One, task_one, &input);
+    time(Task::Two, task_two, &input);
+}
+
+fn task_one(input: &[i32]) -> i32
+{
+    unimplemented!()
+}
+
+fn task_two(input: &[i32]) -> i32
+{
+    unimplemented!()
+}
