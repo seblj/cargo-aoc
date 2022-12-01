060eaf8fe982500a041115c52516a90b38c302dc
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-12-01 10:53:13 +0100

    feat: submit answer

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,33 @@
+use std::fmt::Display;
+
 use clap::ArgMatches;
 
 pub mod file;
+pub mod submit;
+
+#[derive(Debug)]
+pub enum ParseArgError
+{
+    ParseError,
+    TypeError,
+    Invalid(String),
+}
+
+impl Display for ParseArgError
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        match self
+        {
+            ParseArgError::TypeError => write!(f, "Incorrect type found"),
+            ParseArgError::ParseError =>
+            {
+                write!(f, "Couldn't parse input. Check that you are using the correct type")
+            },
+            ParseArgError::Invalid(s) => write!(f, "{}", s),
+        }
+    }
+}
 
 pub fn get_year(matches: &ArgMatches) -> Result<i32, std::num::ParseIntError>
 {
