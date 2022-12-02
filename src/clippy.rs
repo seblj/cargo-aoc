25ae549647a3850ea74ee68a193fc402c05c4192
Sivert Johansen <sivert-joh@hotmail.com>
2022-12-02 10:32:44 +0100

    add clippy subcommand

---
+++ src/clippy.rs
@@ -0,0 +1,32 @@
+use clap::ArgMatches;
+
+pub fn get_day(matches: &ArgMatches) -> Result<u32, std::num::ParseIntError>
+{
+    matches.get_one::<String>("day").unwrap().parse()
+}
+
+pub async fn clippy(matches: &ArgMatches) -> Result<(), Box<dyn std::error::Error>>
+{
+    let day = get_day(matches)?;
+
+    // Creating a vec like this is a bit weird? Seems like there should be
+    // a better solution for this.
+    let mut args = vec!["clippy", "--color", "always"];
+    if matches.get_flag("fix")
+    {
+        // fix complains about unstaged files without the last two flags
+        args.extend(["--fix", "--allow-dirty", "--allow-staged"]);
+    }
+    let day = format!("day_{:02}", day);
+    args.extend(["--bin", &day]);
+
+    let res = tokio::process::Command::new("cargo").args(args).output().await?;
+
+    let err = std::str::from_utf8(&res.stderr)?.trim_end();
+    println!("{}", err);
+
+    let out = std::str::from_utf8(&res.stdout)?.trim_end();
+    println!("{}", out);
+
+    Ok(())
+}
